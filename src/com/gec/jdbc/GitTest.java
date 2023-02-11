32c3e633d28b0aa1f337d1d1d4d1b1a296e8c6e1
ZhouZhanJi <1390524841@qq.com>
2023-02-11 16:52:39 +0800

    second

--- src/com/gec/jdbc/GitTest.java
+++ src/com/gec/jdbc/GitTest.java
@@ -3,5 +3,6 @@ package com.gec.jdbc;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first try");
+        System.out.println("second_one");
     }
 }
